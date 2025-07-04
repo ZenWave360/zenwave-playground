686a4ba896f5cd83f05386f81eb5b4b7ae4cb1ae
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2025-07-04 09:47:04 +0200

    fixes mappers for value objects to plain properties

--- examples/mploed-applicant-scoring/src/main/java/io/zenwave360/example/applicantscoring/service/impl/mappers/ApplicantScoringServiceMapper.java
+++ examples/mploed-applicant-scoring/src/main/java/io/zenwave360/example/applicantscoring/service/impl/mappers/ApplicantScoringServiceMapper.java
@@ -1,34 +1,55 @@
 package io.zenwave360.example.applicantscoring.service.impl.mappers;
 
-import io.zenwave360.example.applicantscoring.service.impl.mappers.BaseMapper;
-import io.zenwave360.example.applicantscoring.domain.*;
-import io.zenwave360.example.applicantscoring.service.dtos.*;
-
-import org.mapstruct.AfterMapping;
-import java.util.List;
+import io.zenwave360.example.applicantscoring.domain.ApplicantScoring;
+import io.zenwave360.example.applicantscoring.service.dtos.ApplicantScoringInput;
+import io.zenwave360.example.applicantscoring.service.dtos.ApplicationNumberInput;
+import io.zenwave360.example.applicantscoring.service.dtos.BalanceAtBankInput;
+import io.zenwave360.example.applicantscoring.service.dtos.CityInput;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
-import org.springframework.data.domain.Page;
 
 @Mapper(uses = { BaseMapper.class })
 public interface ApplicantScoringServiceMapper {
 
     ApplicantScoringServiceMapper INSTANCE = Mappers.getMapper(ApplicantScoringServiceMapper.class);
 
-// input mappings
+    // input mappings
     // ApplicationNumberInputnull-ApplicantScoring createApplicantScoring
-        ApplicantScoring asApplicantScoring(ApplicationNumberInput input);
-    @Mapping(target = "id", ignore = true)ApplicantScoring update(@MappingTarget ApplicantScoring entity, ApplicationNumberInput input);
+    @Mapping(target = "applicationNumber.applicationNumber", source = "applicationNumber")
+    ApplicantScoring asApplicantScoring(ApplicationNumberInput input);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "applicationNumber.applicationNumber", source = "applicationNumber")
+    ApplicantScoring update(@MappingTarget ApplicantScoring entity, ApplicationNumberInput input);
+
     // CityInputnull-ApplicantScoring updateCity
-        ApplicantScoring asApplicantScoring(CityInput input);
-    @Mapping(target = "id", ignore = true)ApplicantScoring update(@MappingTarget ApplicantScoring entity, CityInput input);
+    @Mapping(target = "city.city", source = "city")
+    ApplicantScoring asApplicantScoring(CityInput input);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "city.city", source = "city")
+    ApplicantScoring update(@MappingTarget ApplicantScoring entity, CityInput input);
+
     // BalanceAtBankInputnull-ApplicantScoring updateBalanceAtBank
-        ApplicantScoring asApplicantScoring(BalanceAtBankInput input);
-    @Mapping(target = "id", ignore = true)ApplicantScoring update(@MappingTarget ApplicantScoring entity, BalanceAtBankInput input);
+    @Mapping(target = "balanceAtBank.balanceAtBank", source = "balanceAtBank")
+    ApplicantScoring asApplicantScoring(BalanceAtBankInput input);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "balanceAtBank.balanceAtBank", source = "balanceAtBank")
+    ApplicantScoring update(@MappingTarget ApplicantScoring entity, BalanceAtBankInput input);
+
     // ApplicantScoringInputnull-ApplicantScoring updateApplicantScoring
-        ApplicantScoring asApplicantScoring(ApplicantScoringInput input);
-    @Mapping(target = "id", ignore = true)ApplicantScoring update(@MappingTarget ApplicantScoring entity, ApplicantScoringInput input);
-// output mappings
+    @Mapping(target = "applicationNumber.applicationNumber", source = "applicationNumber")
+    @Mapping(target = "city.city", source = "city")
+    @Mapping(target = "balanceAtBank.balanceAtBank", source = "balanceAtBank")
+    ApplicantScoring asApplicantScoring(ApplicantScoringInput input);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "applicationNumber.applicationNumber", source = "applicationNumber")
+    @Mapping(target = "city.city", source = "city")
+    @Mapping(target = "balanceAtBank.balanceAtBank", source = "balanceAtBank")
+    ApplicantScoring update(@MappingTarget ApplicantScoring entity, ApplicantScoringInput input);
+    // output mappings
 }
--- examples/mploed-applicant-scoring/src/main/java/io/zenwave360/example/applicantscoring/service/impl/mappers/EventsMapper.java
+++ examples/mploed-applicant-scoring/src/main/java/io/zenwave360/example/applicantscoring/service/impl/mappers/EventsMapper.java
@@ -1,19 +1,19 @@
 package io.zenwave360.example.applicantscoring.service.impl.mappers;
 
-import io.zenwave360.example.applicantscoring.service.impl.mappers.BaseMapper;
-import io.zenwave360.example.applicantscoring.domain.*;
-import io.zenwave360.example.applicantscoring.service.dtos.*;
-
-import java.util.List;
+import io.zenwave360.example.applicantscoring.domain.ApplicantScoring;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
-
 
 @Mapper(uses = { BaseMapper.class })
 public interface EventsMapper {
 
     EventsMapper INSTANCE = Mappers.getMapper(EventsMapper.class);
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "version", source = "version")
+    @Mapping(target = "applicationNumber", source = "applicationNumber.applicationNumber")
+    @Mapping(target = "city", source = "city.city")
+    @Mapping(target = "balanceAtBank", source = "balanceAtBank.balanceAtBank")
     io.zenwave360.example.applicantscoring.events.dtos.ApplicantScoringEvent asApplicantScoringEvent(ApplicantScoring applicantScoring);
 }
