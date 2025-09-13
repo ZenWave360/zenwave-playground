973b47720475de926b0eddc7872ad6139381dc39
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2025-09-13 10:30:56 +0200

    mploed work in progress

---
+++ examples/mploed-applicant-scoring/src/main/java/io/zenwave360/example/applicantscoring/config/ExternalizationConfiguration.java
@@ -0,0 +1,9 @@
+package io.zenwave360.example.applicantscoring.config;
+
+import io.zenwave360.modulith.events.scs.config.EnableSpringCloudStreamEventExternalization;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableSpringCloudStreamEventExternalization
+class ExternalizationConfiguration {
+}
