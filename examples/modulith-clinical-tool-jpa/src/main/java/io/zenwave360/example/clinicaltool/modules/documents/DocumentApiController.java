765ec6ec5ce1ab2370bb46b93b23ceb3ecb629b3
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2025-09-18 17:28:48 +0200

    working in the documentation of example projects

--- examples/modulith-clinical-tool-jpa/src/main/java/io/zenwave360/example/clinicaltool/modules/documents/DocumentApiController.java
+++ examples/modulith-clinical-tool-jpa/src/main/java/io/zenwave360/example/clinicaltool/modules/documents/DocumentApiController.java
@@ -1,34 +1,24 @@
 package io.zenwave360.example.clinicaltool.modules.documents;
 
-import io.zenwave360.example.clinicaltool.modules.documents.domain.*;
-import io.zenwave360.example.clinicaltool.modules.documents.*;
-import io.zenwave360.example.clinicaltool.modules.documents.dtos.*;
-import io.zenwave360.example.clinicaltool.modules.documents.*;
-import io.zenwave360.example.clinicaltool.modules.documents.dtos.*;
-import io.zenwave360.example.clinicaltool.modules.documents.mappers.*;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.math.*;
-import java.time.*;
-import java.util.*;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-import org.mapstruct.factory.Mappers;
+import io.zenwave360.example.clinicaltool.modules.documents.dtos.DocumentDataDTO;
+import io.zenwave360.example.clinicaltool.modules.documents.dtos.DocumentInfoDTO;
+import io.zenwave360.example.clinicaltool.modules.documents.mappers.DocumentDTOsMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
+
+import java.util.List;
+import java.util.Optional;
 
 
 /**
@@ -43,46 +33,43 @@ public class DocumentApiController implements DocumentApi {
     @Autowired
     private NativeWebRequest request;
 
-
     private DocumentService documentService;
+
     @Autowired
     public DocumentApiController setDocumentService(DocumentService documentService) {
         this.documentService = documentService;
         return this;
     }
 
-
     private DocumentDTOsMapper mapper = DocumentDTOsMapper.INSTANCE;
 
     public DocumentApiController(DocumentService documentService) {
-        
         this.documentService = documentService;
-        
     }
 
     public Optional<NativeWebRequest> getRequest() {
         return Optional.ofNullable(request);
     }
 
-
-
     @Override
     public ResponseEntity<List<DocumentInfoDTO>> listDocumentInfo(Long documentIds) {
-        log.debug("REST request to listDocumentInfo: {}", documentIds);var documentInfo =  documentService.listDocumentInfo(documentIds);
+        log.debug("REST request to listDocumentInfo: {}", documentIds);
+        var documentInfo = documentService.listDocumentInfo(documentIds);
         var responseDTO = mapper.asDocumentInfoDTOList(documentInfo);
         return ResponseEntity.status(200).body(responseDTO);
     }
 
     @Override
     public ResponseEntity<Resource> downloadDocument(Long id, Boolean preview) {
-        log.debug("REST request to downloadDocument: {}, {}", id, preview);var documentInfo =  documentService.downloadDocument(id);
+        log.debug("REST request to downloadDocument: {}, {}", id, preview);
+        var documentInfo = documentService.downloadDocument(id);
         byte[] bytes = null; // TODO get bytes from documentData.data
         ByteArrayResource resource = new ByteArrayResource(bytes);
         return ResponseEntity
-            .status(200)
-            .header("Content-Disposition", "inline") // or attachment; filename=example.pdf
-            .contentType(MediaType.APPLICATION_OCTET_STREAM) // TODO: set content type
-            .body(resource);
+                .status(200)
+                .header("Content-Disposition", "inline") // or attachment; filename=example.pdf
+                .contentType(MediaType.APPLICATION_OCTET_STREAM) // TODO: set content type
+                .body(resource);
     }
 
     @Override
@@ -96,17 +83,18 @@ public class DocumentApiController implements DocumentApi {
     public ResponseEntity<DocumentInfoDTO> uploadDocument(org.springframework.web.multipart.MultipartFile file, Long id, Integer version, String uuid, String fileName, String documentType, String contentType, List<String> tags, DocumentDataDTO documentData) {
         log.debug("REST request to uploadDocument: {}, {}, {}, {}, {}, {}, {}, {}, {}", file, id, version, uuid, fileName, documentType, contentType, tags, documentData);
         var input = mapper.asDocumentInfo(file, id, version, uuid, fileName, documentType, contentType, tags, documentData);
-    var documentInfo =  documentService.uploadDocument(input);
+        var documentInfo = documentService.uploadDocument(input);
         DocumentInfoDTO responseDTO = mapper.asDocumentInfoDTO(documentInfo);
         return ResponseEntity.status(201).body(responseDTO);
     }
-  protected Pageable pageOf(Integer page, Integer limit, List<String> sort) {
-    Sort sortOrder = sort != null ? Sort.by(sort.stream().map(sortParam -> {
-    String[] parts = sortParam.split(":");
-    String property = parts[0];
-    Sort.Direction direction = parts.length > 1 ? Sort.Direction.fromString(parts[1]) : Sort.Direction.ASC;
-    return new Sort.Order(direction, property);
-    }).toList()) : Sort.unsorted();
-    return PageRequest.of(page != null ? page : 0, limit != null ? limit : 10, sortOrder);
-  }
+
+    protected Pageable pageOf(Integer page, Integer limit, List<String> sort) {
+        Sort sortOrder = sort != null ? Sort.by(sort.stream().map(sortParam -> {
+            String[] parts = sortParam.split(":");
+            String property = parts[0];
+            Sort.Direction direction = parts.length > 1 ? Sort.Direction.fromString(parts[1]) : Sort.Direction.ASC;
+            return new Sort.Order(direction, property);
+        }).toList()) : Sort.unsorted();
+        return PageRequest.of(page != null ? page : 0, limit != null ? limit : 10, sortOrder);
+    }
 }
